8cf7199f51c1d36a46613d9e2352b62692fc3eac
AhmadAyman1999 <48836158+AhmadAyman1999@users.noreply.github.com>
2020-04-05 16:27:07 +0200

    Add files via upload

--- ScannerProject/ScannerProject/import.Designer.cs
+++ ScannerProject/ScannerProject/import.Designer.cs
@@ -34,6 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,14 +44,14 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(328, 36);
+            this.textBox1.Size = new System.Drawing.Size(558, 36);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(469, 105);
+            this.button1.Location = new System.Drawing.Point(709, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(157, 36);
             this.button1.TabIndex = 2;
@@ -61,7 +62,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(469, 189);
+            this.button2.Location = new System.Drawing.Point(68, 388);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(157, 36);
             this.button2.TabIndex = 3;
@@ -74,7 +75,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(259, 9);
+            this.label1.Location = new System.Drawing.Point(386, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(208, 58);
             this.label1.TabIndex = 4;
@@ -83,25 +84,38 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(68, 189);
+            this.textBox2.Location = new System.Drawing.Point(68, 172);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(328, 235);
+            this.textBox2.Size = new System.Drawing.Size(558, 185);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(681, 172);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(224, 185);
+            this.textBox3.TabIndex = 6;
+            // 
             // import
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(730, 450);
+            this.ClientSize = new System.Drawing.Size(956, 442);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -123,5 +137,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- ScannerProject/ScannerProject/write.cs
+++ ScannerProject/ScannerProject/write.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,41 +11,42 @@ using System.Windows.Forms;
 
 namespace ScannerProject
 {
-    public partial class import : Form
+    public partial class write : Form
     {
         FileReader file = new FileReader();
         Scanner scanner;
         string text = "Lexeme \t\t\t Token" + Environment.NewLine + Environment.NewLine;
+        string path = "";
         
-        public import()
+        public write()
         {
             InitializeComponent();
         }
 
-        private void import_Load(object sender, EventArgs e)
+        private void write_Load(object sender, EventArgs e)
         {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DialogResult dr = openFileDialog1.ShowDialog();
-            if (dr == System.Windows.Forms.DialogResult.OK)
-            {
-                textBox1.Text = openFileDialog1.FileName;
-                file.readAllFile(textBox1.Text);
-                textBox2.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
-            }
+            path += textBox1.Text;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(textBox1.Text))
+
+                path = "code.txt";
+
+            else
+                path += "\\code.txt";
+
+            StreamWriter newfile = new StreamWriter(path);
+            newfile.Write(textBox2.Text);
+            newfile.Close();
+            file.readAllFile(path);
             display();
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
         }
 
         void display()
@@ -59,22 +61,25 @@ namespace ScannerProject
             foreach (var lexeme in scanner.ScannedList)
             {
 
-
-                //textBox2.Text = String.Join(Environment.NewLine, scanner.ScannedList);
-
                 text += String.Format("{0} \t\t\t {1}", lexeme.Key.Trim('\0'), lexeme.Value);
                 text += Environment.NewLine;
+
             }
-            if (scanner.checkUnmatchedBraces()) text += "Unmatched Braces";
+            //if (scanner.checkUnmatchedBraces()) text += "Unmatched Braces";
             textBox2.Text = text;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
